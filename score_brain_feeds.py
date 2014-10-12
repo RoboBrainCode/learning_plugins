9e3a2a1952819082c9bd29035622b7de92890401
ashesh <asheshjain399@gmail.com>
2014-10-12 15:42:43 -0700

    updated score function

--- score_brain_feeds.py
+++ score_brain_feeds.py
@@ -5,13 +5,13 @@ import numpy as np
 import importlib
 import sys
 sys.path.insert(0,'/var/www/Backend/Backend/')
-#import settings as setfile
-
-#host = setfile.DATABASES['default']['HOST']
-#dbname = setfile.DATABASES['default']['NAME']
-#port = int(setfile.DATABASES['default']['PORT'])
 
 def readConfigFile():
+    """
+        Reading the setting file to use.
+        Different setting files are used on Production and Test robo brain
+    """
+
     global setfile
     config = ConfigParser.ConfigParser()
     config.read('/tmp/backend_uwsgi_setting')
@@ -20,6 +20,10 @@ def readConfigFile():
     setfile = importlib.import_module(setting_file_name)	
 
 def updatescore():
+    """
+        Updates BrainFeed table score field
+    """
+
     set_params()
     client = pm.MongoClient(host,port)
     db = client[dbname]
@@ -33,7 +37,7 @@ def updatescore():
         inverse_feed_weight = feeds['log_normalized_feed_show']
         new_score = strategy_2(project_name, netvotes, created_at, project_weights, inverse_feed_weight)
         print "old score ",feeds['score']
-        print feeds['_id']
+        print "{0} {1}".format(feeds['source_url'],feeds['_id'])
         print "new score ",new_score
         print "*************************************"
         brain_feeds.update({'_id':feeds['_id']},{'$set':{'score' : new_score, 'update_score' : False}},upsert=False,multi=False)
@@ -99,9 +103,11 @@ def strategy_2(project_name, netvotes, created_at, project_weights, inverse_feed
 
 if __name__ == "__main__":
     global host, dbname, port, setfile
-    readConfigFile()
-    updatescore()
 
+    # Reading the setting file for db address
+    readConfigFile()
     host = setfile.DATABASES['default']['HOST']
     dbname = setfile.DATABASES['default']['NAME']
     port = int(setfile.DATABASES['default']['PORT'])
+
+    updatescore()

a77e0fded92047f6ec2addbe166c80583bf4b93f
Arzav Jain <arzavj@stanford.edu>
2014-12-02 13:37:25 -0800

    updated mongo ip with elastic ip and factored node beliefs out

--- belief_crawler.py
+++ belief_crawler.py
@@ -4,9 +4,10 @@ from py2neo import cypher, neo4j
 import pymongo as pm
 import numpy as np
 import math
+import argparse
 
 GRAPH_DB_URL = "http://ec2-54-187-76-157.us-west-2.compute.amazonaws.com:7474/db/data/"
-DB_HOST = 'ec2-54-69-241-26.us-west-2.compute.amazonaws.com'
+DB_HOST = 'ec2-54-148-208-139.us-west-2.compute.amazonaws.com'
 DB_NAME = 'backend_test_deploy'
 DB_PORT = 27017
 
@@ -58,11 +59,35 @@ def get_belief_score(node_record):
     normalize_feature_vector(feature_vector)
     return logistic_function(np.inner(feature_vector, weight_vector))
 
-def set_belief_score(belief_score, node_id):
+def set_node_belief(belief_score, node_id):
     print node_id, belief_score
     q = neo4j.CypherQuery(GRAPH_DB, 
         "MATCH (n { id: {nid}}) SET n.belief = {b_score}")
     q.run(nid=node_id, b_score=belief_score)
+
+def crawl_nodes():
+    query = neo4j.CypherQuery(GRAPH_DB, "START n=node(*) RETURN n")
+    for record in query.stream():
+        belief = get_belief_score(record[0])
+        set_node_belief(belief, record[0]['id'])
+
+def crawl_edges():
+    query = neo4j.CypherQuery(GRAPH_DB, "")
+    for record in query.stream():
+        belief = get_belief_score(record[0])
+        set_node_belief(belief, record[0]['id'])
+
+def parse_args():
+    """
+    :return: arguments passed in on the command line as a dict. For arguments
+        that are not specified, default values are inserted into the dict.
+    """
+    parser = argparse.ArgumentParser(
+        description='Crawls nodes or edges of the graph to add beliefs.')
+    parser.add_argument('-c', help='entity to crawl - edges (default) or nodes',
+        default='edges', type=str)
+    args = vars(parser.parse_args())
+    return args
 
 def main():
     global BRAIN_FEEDS, GRAPH_DB
@@ -71,10 +96,10 @@ def main():
     db = client[DB_NAME]
     BRAIN_FEEDS = db['brain_feeds']
     GRAPH_DB = neo4j.GraphDatabaseService(GRAPH_DB_URL)
-    query = neo4j.CypherQuery(GRAPH_DB, "MATCH (n) RETURN n")
-    for record in query.stream():
-        belief = get_belief_score(record[0])
-        set_belief_score(belief, record[0]['id'])
+    if parse_args()['c'] == 'edges':
+        crawl_edges()
+    else:
+        crawl_nodes()
 
 if __name__ == '__main__':
     main()
